d3e6441c7960ccfd926ca0f606a015cfc51c97a4
Dan <DanRhysMorris@gmail.com>
2022-10-16 12:07:06 +0100

    Add additional equality operators to Option

--- Util/include/Util/Option.hpp
+++ Util/include/Util/Option.hpp
@@ -16,10 +16,10 @@ template<typename T>
 class Option
 {
 public:
-    Option()
-        : mHasValue(false)
-        , mData{}
-    {}
+	Option()
+		: mHasValue(false)
+		, mData{}
+	{}
 
 
 
@@ -33,134 +33,196 @@ public:
 
 
 
-    Option(const Option& rhs) requires ( std::is_copy_constructible_v<T> )
-    : mHasValue(rhs.mHasValue)
-    , mData{}
-    {
+	Option(const Option& rhs) requires ( std::is_copy_constructible_v<T> )
+	: mHasValue(rhs.mHasValue)
+	, mData{}
+	{
 		if (rhs)
 		{
 			new(mData) T(*rhs);
 		}
-    }
+	}
 
 
 
-    Option(Option&& rhs)  noexcept requires ( std::is_move_constructible_v<T> )
-    : mHasValue(false)
-    , mData{}
-    {
-        if (rhs)
-        {
+	Option(Option&& rhs)  noexcept requires ( std::is_move_constructible_v<T> )
+	: mHasValue(false)
+	, mData{}
+	{
+		if (rhs)
+		{
 			new (mData) T(std::move(*rhs));
 			mHasValue = Replace(rhs.mHasValue, false);
-	        std::fill(rhs.mData, rhs.mData + sizeof(T), 0);
+			std::fill(rhs.mData, rhs.mData + sizeof(T), 0);
 		}
-    }
+	}
 
 
 
-    Option& operator=(const Option& rhs) requires ( std::is_copy_assignable_v<T> )
-    {
-        if (this != &rhs)
-        {
-            if (mHasValue)
-            {
-                (*this)->~T();
-            }
+	Option& operator=(const Option& rhs) requires ( std::is_copy_assignable_v<T> )
+	{
+		if (this != &rhs)
+		{
+			if (mHasValue)
+			{
+				(*this)->~T();
+			}
 
-            mHasValue = rhs.mHasValue;
-            if (mHasValue)
-            {
-                new(mData) T(*rhs);
-            }
-        }
+			mHasValue = rhs.mHasValue;
+			if (mHasValue)
+			{
+				new(mData) T(*rhs);
+			}
+		}
 
-        return *this;
-    }
+		return *this;
+	}
 
-    Option& operator=(Option&& rhs) noexcept requires ( std::is_move_assignable_v<T> )
-    {
-        if (this != &rhs)
-        {
-            if (mHasValue)
-            {
-                (*this)->~T();
-	            std::fill(mData, mData + sizeof(T), 0);
-            }
 
-            mHasValue = rhs.mHasValue;
-            if (mHasValue)
-            {
-                new (mData) T(std::move(*rhs));
-                rhs.mHasValue = false;
+
+	Option& operator=(Option&& rhs) noexcept requires ( std::is_move_assignable_v<T> )
+	{
+		if (this != &rhs)
+		{
+			if (mHasValue)
+			{
+				(*this)->~T();
+				std::fill(mData, mData + sizeof(T), 0);
+			}
+
+			mHasValue = rhs.mHasValue;
+			if (mHasValue)
+			{
+				new (mData) T(std::move(*rhs));
+				rhs.mHasValue = false;
 				std::fill(rhs.mData, rhs.mData + sizeof(T), 0);
-            }
-        }
+			}
+		}
 
-        return *this;
-    }
-
-    ~Option()
-    {
-        if (mHasValue)
-        {
-            (*this)->~T();
-        }
-    }
+		return *this;
+	}
 
 
-    template<typename ...Args>
-    void Emplace(Args ...args)
-    {
-        if (mHasValue)
-        {
-            (*this)->~T();
-        }
 
-        new (mData) T(std::forward<Args>(args)...);
-        mHasValue = true;
-    }
-
-    void Reset()
-    {
-        if (mHasValue)
-        {
-            (*this)->~T();
-            mHasValue = false;
-        }
-    }
+	~Option()
+	{
+		if (mHasValue)
+		{
+			(*this)->~T();
+		}
+	}
 
 
-    inline bool HasValue() const { return mHasValue; }
-    explicit inline operator bool() const { return mHasValue; }
+	template<typename ...Args>
+	void Emplace(Args ...args)
+	{
+		if (mHasValue)
+		{
+			(*this)->~T();
+		}
+
+		new (mData) T(std::forward<Args>(args)...);
+		mHasValue = true;
+	}
+
+	void Reset()
+	{
+		if (mHasValue)
+		{
+			(*this)->~T();
+			mHasValue = false;
+		}
+	}
 
 
-          T& operator *()       { Assert(mHasValue); return *reinterpret_cast<      T*>(mData); }
-    const T& operator *() const { Assert(mHasValue); return *reinterpret_cast<const T*>(mData); }
-
-          T* operator->()       { Assert(mHasValue); return  reinterpret_cast<      T*>(mData); }
-    const T* operator->() const { Assert(mHasValue); return  reinterpret_cast<const T*>(mData); }
+	inline bool HasValue() const { return mHasValue; }
+	explicit inline operator bool() const { return mHasValue; }
 
 
-    bool operator==(const Option<T>& rhs) const requires ( std::equality_comparable<T> )
-    {
-        if (!HasValue() && !rhs.HasValue())
-        {
-            return true;
-        } else if (HasValue() && rhs.HasValue())
-        {
-            return (**this) == (*rhs);
-        }
+		  T& operator *()       { Assert(mHasValue); return *reinterpret_cast<      T*>(mData); }
+	const T& operator *() const { Assert(mHasValue); return *reinterpret_cast<const T*>(mData); }
 
-        return false;
-    }
+		  T* operator->()       { Assert(mHasValue); return  reinterpret_cast<      T*>(mData); }
+	const T* operator->() const { Assert(mHasValue); return  reinterpret_cast<const T*>(mData); }
 
-    inline bool operator!=(const Option<T>& rhs) const requires ( std::equality_comparable<T> )
-    {
-        return !((*this) == rhs);
-    }
+
+	bool operator==(const Option<T>& rhs) const requires ( std::equality_comparable<T> )
+	{
+		if (!HasValue() && !rhs.HasValue())
+		{
+			return true;
+		}
+		else if (HasValue() && rhs.HasValue())
+		{
+			return (**this) == (*rhs);
+		}
+
+		return false;
+	}
+
+
+
+	inline bool operator!=(const Option<T>& rhs) const requires ( std::equality_comparable<T> )
+	{
+		if (!HasValue() && !rhs.HasValue())
+		{
+			return false;
+		}
+		else if (HasValue() && rhs.HasValue())
+		{
+			return (**this) != (*rhs);
+		}
+
+		return true;
+	}
+
+
+
+	inline bool operator==(const T& rhs) const requires ( std::equality_comparable<T> )
+	{
+		if (!HasValue())
+		{
+			return false;
+		}
+		else
+		{
+			return (*this) == rhs;
+		}
+	}
+
+
+
+	inline bool operator!=(const T& rhs) const requires ( std::equality_comparable<T> )
+	{
+		if (!HasValue())
+		{
+			return true;
+		}
+		else
+		{
+			return (*this) != rhs;
+		}
+	}
+
+
 
 private:
-    bool    mHasValue;
-    uint8_t mData[sizeof(T)];
+	bool    mHasValue;
+	uint8_t mData[sizeof(T)];
 };
+
+
+
+template <typename T>
+inline bool operator==(const T& a, const Option<T>& b) requires ( std::equality_comparable<T> )
+{
+	return b == a;
+}
+
+
+
+template <typename T>
+inline bool operator!=(const T& a, const Option<T>& b) requires ( std::equality_comparable<T> )
+{
+	return b != a;
+}

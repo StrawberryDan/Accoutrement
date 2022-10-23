de38f816c09679122ef9c78b9f98323512f0d921
daniel <DanRhysMorris@gmail.com>
2022-10-23 19:39:09 +0100

    Improve formatting in Macros.hpp

--- Util/include/Util/Macros.hpp
+++ Util/include/Util/Macros.hpp
@@ -3,7 +3,7 @@
 
 
 #if __cpp_lib_unreachable
-#define UNREACHABLE std::unreachable()
+	#define UNREACHABLE std::unreachable()
 #elif __clang__
-#define UNREACHABLE __builtin_unreachable()
+	#define UNREACHABLE __builtin_unreachable()
 #endif

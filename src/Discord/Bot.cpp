b324d519eb59887c07207312875c88b4cf527532
StrawberryDan <DanRhysMorris@gmail.com>
2023-08-07 23:50:17 +0100

    Make voice ahead buffer time shorter to make UI feel snappier.

--- src/Discord/Bot.cpp
+++ src/Discord/Bot.cpp
@@ -72,7 +72,7 @@ namespace Strawberry::Accoutrement
 	{
 		mAudioSendingThread.Emplace([
 			this,
-			clock = Core::Metronome(0.02, 0.50)
+			clock = Core::Metronome(0.02, 0.6)
 		](Core::RepeatingTask* thread) mutable	{
 			if (clock)
 			{

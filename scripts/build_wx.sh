06fa41f5a4dcfb16ef1eae8608aa90e85140020b
StrawberryDan <DanRhysMorris@gmail.com>
2023-08-11 12:35:00 +0100

    Switch to just using the main branch version of wxWidgets.

--- scripts/build_wx.sh
+++ scripts/build_wx.sh
@@ -6,8 +6,8 @@ SOURCE_DIR=$1
 
 if [[ ! -d  ${SOURCE_DIR} ]]; then
   git clone --recurse-submodules https://github.com/wxWidgets/wxWidgets.git ${SOURCE_DIR}
-  cd ${SOURCE_DIR} || exit
-  git checkout --force --recurse-submodules "v3.2.2"
+  # cd ${SOURCE_DIR} || exit
+  # git checkout --force --recurse-submodules "v3.2.2.1"
 fi
 
 

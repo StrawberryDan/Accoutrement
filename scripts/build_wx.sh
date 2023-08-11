16cd9380213783434c96b142c91489c0d4eb7296
StrawberryDan <DanRhysMorris@gmail.com>
2023-08-11 11:58:55 +0100

    Simplify wxWidgets source code cloning structure
    The directory structure for cloning wxWidgets source code has been simplified. Previously, the source was being cloned into a 'source' subdirectory and separate 'build' and 'lib' directories were being manually created. This appeared to be unnecessary and was confusing to understand. Now the source is directly cloned into the 'wxWidgets' directory, improving directory structure clarity and project setup simplicity.

--- scripts/build_wx.sh
+++ scripts/build_wx.sh
@@ -5,12 +5,8 @@ SOURCE_DIR=$1
 
 
 if [[ ! -d  ${SOURCE_DIR} ]]; then
-  git clone --recurse-submodules https://github.com/wxWidgets/wxWidgets.git ${SOURCE_DIR}/source
-  mkdir -p ${SOURCE_DIR}/build
-  mkdir -p ${SOURCE_DIR}/install/lib
-
-
-  cd ${SOURCE_DIR}/source || exit
+  git clone --recurse-submodules https://github.com/wxWidgets/wxWidgets.git ${SOURCE_DIR}
+  cd ${SOURCE_DIR} || exit
   git checkout --force --recurse-submodules "v3.2.2"
 fi
 

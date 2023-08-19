a17765ffc86fdb53791823a12e65e602269f3950
StrawberryDan <DanRhysMorris@gmail.com>
2023-08-19 15:10:16 +0100

    Implement SoundDatabase

---
+++ src/UI/Model/SoundDatabase.cpp
@@ -0,0 +1,41 @@
+//======================================================================================================================
+//  Includes
+//----------------------------------------------------------------------------------------------------------------------
+#include "SoundDatabase.hpp"
+
+//======================================================================================================================
+//  Class Definitions
+//----------------------------------------------------------------------------------------------------------------------
+namespace Strawberry::Accoutrement
+{
+	static Core::Mutex<Core::Option<Core::Mutex<SoundDatabase>>> sInstance;
+
+	Core::MutexGuard<SoundDatabase> SoundDatabase::Get()
+	{
+		auto lock = sInstance.Lock();
+		if (!lock->HasValue()) { (*lock) = Core::Mutex(SoundDatabase()); }
+		return lock->Value().Lock();
+	}
+
+	SoundDatabase::Id SoundDatabase::AddSound(Sound sound)
+	{
+		auto id = mNextId++;
+		mSounds.emplace(id, std::move(sound));
+		return id;
+	}
+
+	void SoundDatabase::RemoveSound(SoundDatabase::Id id)
+	{
+		mSounds.erase(id);
+	}
+
+	const Core::Option<const Sound*> SoundDatabase::GetSound(SoundDatabase::Id id) const
+	{
+		return mSounds.contains(id) ? Core::Option(&mSounds.at(id)) : Core::NullOpt;
+	}
+
+	size_t SoundDatabase::Count() const
+	{
+		return mSounds.size();
+	}
+} // namespace Strawberry::Accoutrement
---
+++ src/UI/Model/SoundDatabase.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+//======================================================================================================================
+//  Includes
+//----------------------------------------------------------------------------------------------------------------------
+// This Project
+#include "Sound.hpp"
+// Core
+#include "Strawberry/Core/Sync/Mutex.hpp"
+#include "Strawberry/Core/Util/Option.hpp"
+// Standard Library
+#include <cstddef>
+#include <map>
+
+//======================================================================================================================
+//  Class Declaration
+//----------------------------------------------------------------------------------------------------------------------
+namespace Strawberry::Accoutrement
+{
+	class SoundDatabase
+	{
+	public:
+		static Core::MutexGuard<SoundDatabase> Get();
+
+	public:
+		using Id = size_t;
+
+	public:
+		/// Adds a new sound to the database. Returns the id of the added sound.
+		Id                               AddSound(Sound sound);
+		/// Removes the sound associated with the given Id.
+		void                             RemoveSound(Id id);
+		/// Returns an pointer to the sound with the given id if it exists.
+		const Core::Option<const Sound*> GetSound(Id id) const;
+		/// Returns the number of sounds in the database.
+		size_t                           Count() const;
+
+	private:
+		SoundDatabase() = default;
+
+	private:
+		Id                  mNextId = 0;
+		std::map<Id, Sound> mSounds;
+	};
+} // namespace Strawberry::Accoutrement

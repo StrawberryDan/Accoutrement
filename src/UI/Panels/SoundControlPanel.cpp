3bfbdf399c44617919a613581a339dcf6ca97296
StrawberryDan <DanRhysMorris@gmail.com>
2023-09-18 11:25:43 +0100

    Implemented functionality of SoundControlPanel

--- src/UI/Panels/SoundControlPanel.cpp
+++ src/UI/Panels/SoundControlPanel.cpp
@@ -2,34 +2,102 @@
 //  Includes
 //----------------------------------------------------------------------------------------------------------------------
 #include "SoundControlPanel.hpp"
+#include "../../Discord/Bot.hpp"
 // WxWidgets
+#include "../../Model/SoundDatabase.hpp"
 #include "wx/button.h"
 #include "wx/gbsizer.h"
 #include "wx/listctrl.h"
-
 
 //======================================================================================================================
 //  Class Definitions
 //----------------------------------------------------------------------------------------------------------------------
 namespace Strawberry::Accoutrement
 {
-	SoundControlPanel::SoundControlPanel(wxWindow* parent)
+	enum : wxWindowID
+	{
+		REMOVE_SOUND = wxID_HIGHEST,
+		REPEAT_SOUND
+	};
+
+	wxBEGIN_EVENT_TABLE(SoundControlPanel, wxPanel) EVT_BUTTON(REMOVE_SOUND, SoundControlPanel::OnRemoveSound)
+		EVT_BUTTON(REPEAT_SOUND, SoundControlPanel::OnRepeatSound) wxEND_EVENT_TABLE()
+
+			SoundControlPanel::SoundControlPanel(wxWindow* parent)
 		: wxPanel(parent)
 	{
 		auto sizer = new wxGridBagSizer();
 
-		auto list  = new wxListCtrl(this, wxID_ANY);
-		sizer->Add(list, {0, 0}, {2, 1}, wxALL | wxEXPAND, 5);
+		mList      = new wxListCtrl(this, wxID_ANY);
+		mList->SetWindowStyleFlag(wxLC_LIST);
+		sizer->Add(mList, {0, 0}, {2, 1}, wxALL | wxEXPAND, 5);
 
-		auto removeButton = new wxButton(this, wxID_ANY, "Remove");
-		sizer->Add(removeButton, {0, 1}, {1, 1}, wxALL | wxALIGN_CENTER, 5);
+		mRemoveButton = new wxButton(this, REMOVE_SOUND, "Remove");
+		sizer->Add(mRemoveButton, {0, 1}, {1, 1}, wxALL | wxALIGN_CENTER, 5);
 
-		auto repeatButton = new wxButton(this, wxID_ANY, "Repeat");
-		sizer->Add(repeatButton, {1, 1}, {1, 1}, wxALL | wxALIGN_CENTER, 5);
+		mRepeatButton = new wxButton(this, REPEAT_SOUND, "Repeat");
+		sizer->Add(mRepeatButton, {1, 1}, {1, 1}, wxALL | wxALIGN_CENTER, 5);
 
 		sizer->AddGrowableRow(0);
 		sizer->AddGrowableRow(1);
 		sizer->AddGrowableCol(0);
 		SetSizerAndFit(sizer);
+
+		if (Bot::Get()) Bot::Get()->GetSoundPlayer().Lock()->Register(this);
+	}
+
+	void SoundControlPanel::Receive(BotStartedRunningEvent value)
+	{
+		Bot::Get()->GetSoundPlayer().Lock()->Register(this);
+	}
+
+	void SoundControlPanel::Receive(BotStoppedRunningEvent value)
+	{
+		Bot::Get()->GetSoundPlayer().Lock()->Unregister(this);
+	}
+
+	void SoundControlPanel::Receive(SoundPlayerEvent::SoundStarted value)
+	{
+		auto itemID = mList->InsertItem(mList->GetItemCount(), value.sound->GetName());
+		mList->SetItemPtrData(itemID, value.soundID);
+
+		mList->SetItemTextColour(itemID, value.repeating ? *wxBLACK : mList->GetTextColour());
+		mList->SetItemBackgroundColour(itemID, value.repeating ? *wxGREEN : mList->GetBackgroundColour());
+	}
+
+	void SoundControlPanel::Receive(SoundPlayerEvent::SoundEnded value)
+	{
+		for (int i = 0; i < mList->GetItemCount(); i++)
+		{
+			if (value.songID == mList->GetItemData(i)) { mList->DeleteItem(i); }
+		}
+	}
+
+	void SoundControlPanel::Receive(SoundPlayerEvent::SoundRepeat value)
+	{
+		for (int i = 0; i < mList->GetItemCount(); i++)
+		{
+			if (value.songID == mList->GetItemData(i)) { mList->SetItemTextColour(i, value.repeating ? *wxBLACK : mList->GetTextColour()); }
+			if (value.songID == mList->GetItemData(i)) { mList->SetItemBackgroundColour(i, value.repeating ? *wxGREEN : mList->GetBackgroundColour()); }
+		}
+	}
+
+	void SoundControlPanel::OnRemoveSound(wxCommandEvent& event)
+	{
+		auto selected = mList->GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
+		if (selected == -1) return;
+
+		auto soundID = mList->GetItemData(selected);
+		mList->DeleteItem(selected);
+		Bot::Get()->GetSoundPlayer().Lock()->RemoveSound(soundID);
+	}
+
+	void SoundControlPanel::OnRepeatSound(wxCommandEvent& event)
+	{
+		auto selected = mList->GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
+		if (selected == -1) return;
+
+		auto soundID = mList->GetItemData(selected);
+		Bot::Get()->GetSoundPlayer().Lock()->SetRepeat(soundID, !Bot::Get()->GetSoundPlayer().Lock()->GetRepeat(soundID));
 	}
 } // namespace Strawberry::Accoutrement
--- src/UI/Panels/SoundControlPanel.hpp
+++ src/UI/Panels/SoundControlPanel.hpp
@@ -4,18 +4,44 @@
 //======================================================================================================================
 //  Includes
 //----------------------------------------------------------------------------------------------------------------------
+// This Project
+#include "../../Model/SoundPlayer.hpp"
+#include "../Events/BotStartedRunningEvent.hpp"
+#include "../Events/BotStoppedRunningEvent.hpp"
+// Core
+#include "Strawberry/Core/IO/ChannelReceiver.hpp"
+#include "Strawberry/Core/IO/Receiver.hpp"
 // WxWidgets
+#include "wx/listctrl.h"
 #include "wx/panel.h"
-
 
 //======================================================================================================================
 //  Class Declaration
 //----------------------------------------------------------------------------------------------------------------------
 namespace Strawberry::Accoutrement
 {
-	class SoundControlPanel : public wxPanel
+	class SoundControlPanel
+		: public wxPanel
+		, public Core::IO::ChannelReceiver<SoundPlayerEvent::SoundStarted, SoundPlayerEvent::SoundEnded, SoundPlayerEvent::SoundRepeat>
+		, public Core::IO::Receiver<BotStartedRunningEvent, BotStoppedRunningEvent>
 	{
+		wxDECLARE_EVENT_TABLE();
+
 	public:
 		explicit SoundControlPanel(wxWindow* parent);
+
+	protected:
+		void Receive(BotStartedRunningEvent value) override;
+		void Receive(BotStoppedRunningEvent value) override;
+		void Receive(SoundPlayerEvent::SoundStarted value) override;
+		void Receive(SoundPlayerEvent::SoundEnded value) override;
+		void Receive(SoundPlayerEvent::SoundRepeat value) override;
+		void OnRemoveSound(wxCommandEvent& event);
+		void OnRepeatSound(wxCommandEvent& event);
+
+	private:
+		wxListCtrl* mList         = nullptr;
+		wxButton*   mRemoveButton = nullptr;
+		wxButton*   mRepeatButton = nullptr;
 	};
 } // namespace Strawberry::Accoutrement
